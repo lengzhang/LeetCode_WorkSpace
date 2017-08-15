9bf053edf8a6f2bb6b45a5f21c5e47c43c6ab77d
Leng <lengzhang@live.com>
2017-08-15 13:37:55 -0700

    rm error part

--- Regular Expression Matching/main.cpp
+++ Regular Expression Matching/main.cpp
@@ -16,29 +16,7 @@ int main(){
 };
 
 bool isMatch(std::string s, std::string p) {
-    std::map<char, int> alphabets_map;
-    for (int i = 0; i < p.size() - s.size() + 1; i++) {
-        if (p[i] == s[0] || p[i] == '.' || (p[i] == '*' && alphabets_map.find(s[0]) != alphabets_map.end())) {
-            std::map<char, int> temp_map = alphabets_map;
-            printf("%d - %d\n", alphabets_map.size(), temp_map.size());
-            for (int j = 1; j < s.size(); j++) {
-                
-                if (s[j] != p[i + j] && p[i + j] != '.')
-                    break;
-                else if (p[i + j] == '*' && temp_map.find(s[j]) == temp_map.end())
-                    break;
-                printf("%c[%c]", s[j], p[i+j]);
-                if (temp_map.find(p[i]) == temp_map.end()) {
-                    temp_map[p[i]] = 1;
-                }
-            }
-            printf("\n");
-        }
-
-        if (alphabets_map.find(p[i]) == alphabets_map.end()) {
-            alphabets_map[p[i]] = 1;
-        }
-    }
+    
     
     return false;
 };
